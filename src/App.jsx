85008682ff01f5ebc29ea631b1b344de0bac4214
NAMGYEONGCHO <official2002mk@gmail.com>
2023-03-01 17:49:25 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Room Occupancy Calculator</h1>
+      <p>
+        <label>
+          Premium Rooms:
+        </label>
+      </p>
+      <p>
+        <label>
+          Economy Rooms:
+        </label>
+      </p>
+      <h2>Occupancy</h2>
+      
+    </div>
+  );
+}
+
+export default App;
